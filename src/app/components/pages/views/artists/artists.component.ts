095b2d78eae65eeded5b6b37e7de5d4fd65e9fcc
Oscar <oscarrc.web@gmail.com>
2020-09-10 12:26:36 +0200

    [MOD] Update pagination getter function

--- src/app/components/pages/views/artists/artists.component.ts
+++ src/app/components/pages/views/artists/artists.component.ts
@@ -25,15 +25,9 @@ export class ArtistsComponent implements OnInit, AfterViewInit {
                 private loadingService: LoadingService,
                 private artistService: ArtistService) {
                     this.routeSubscription = this.route.params.subscribe(param => {
-                        let similar = route.snapshot.url.some( s => { return s.path == "similar" });
-
-                        if (param.id && similar) {
-                            this.id = param.id;
-                            this.initSimilar();
-                        }else if(param.name){         
-                            this.name = param.name;              
-                            this.initArtists();
-                        }
+                        this.id = param.id;       
+                        this.name = param.name;
+                        this.getContent();
                     });
                 }
 
@@ -46,22 +40,30 @@ export class ArtistsComponent implements OnInit, AfterViewInit {
 
     nextPage(){
         this.page = this.page + 1;
-        this.initArtists();
+        this.getContent();
     }
 
     prevPage(){
         this.page = this.page - 1;
-        this.initArtists();
+        this.getContent();
     }
 
-    initArtists() {
+    getContent(){
+        if(this.id){
+            this.getSimilar()
+        }else if(this.name){
+            this.getArtists()
+        }
+    }
+
+    getArtists() {
         this.artistService.getTop(this.country, this.page, this.limit).subscribe(
             res => this.artists = res,
             err => console.log(err)
         )
     }
 
-    initSimilar() {
+    getSimilar() {
         this.artistService.getSimilar(this.id).subscribe(
             res => this.artists = res,
             err => console.log(err)
