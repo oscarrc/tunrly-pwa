f74f7c8d7a3cabf98528096b7ce6dac425f87f93
Oscar <oscarrc.web@gmail.com>
2020-10-14 20:02:00 +0200

    [ADD] User profile basic layout

--- src/app/components/pages/views/user/profile/profile.component.ts
+++ src/app/components/pages/views/user/profile/profile.component.ts
@@ -1,32 +1,31 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, OnDestroy } from '@angular/core';
 
 import { LoadingService } from '../../../../../services/loading.service';
+import { UserService } from '../../../../../services/user.service';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-user-profile',
     templateUrl: './profile.component.html'
 })
-export class UserProfileComponent implements OnInit, AfterViewInit {
+export class UserProfileComponent implements OnInit, AfterViewInit, OnDestroy {
 
     userProfile: any;
-
-    constructor(private loadingService: LoadingService) { }
+    private userSubscription: Subscription;
+    
+    constructor(private loadingService: LoadingService, private userService: UserService) { }
 
     ngOnInit() {
-        // This is static data just to display replace with your data
-        this.userProfile = {
-            image: './assets/images/users/thumb.jpg',
-            name: 'Halo Admin',
-            firstName: 'Halo',
-            lastName: 'Admin',
-            userRole: 'Admin',
-            location: 'USA',
-            description: ''
-        };
+        this.userSubscription = this.userService.user.subscribe( user => {
+            this.userProfile = user;
+        });
     }
 
     ngAfterViewInit() {
         this.loadingService.stopLoading();
     }
 
+    ngOnDestroy() {
+        this.userSubscription.unsubscribe();
+    }
 }
