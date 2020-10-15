f93e3fe12ac558f6e66390f0c66b84461c328159
Oscar <oscarrc.web@gmail.com>
2020-10-15 19:10:07 +0200

    [WIP] Settings and profile views

--- src/app/components/pages/views/user/profile/profile.component.ts
+++ src/app/components/pages/views/user/profile/profile.component.ts
@@ -3,6 +3,10 @@ import { AfterViewInit, Component, OnInit, OnDestroy } from '@angular/core';
 import { LoadingService } from '../../../../../services/loading.service';
 import { UserService } from '../../../../../services/user.service';
 import { Subscription } from 'rxjs';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { AvailabilityValidator } from '../../../../../core/validators/availability.validator';
+import { PasswordValidator } from '../../../../../core/validators/password.validator';
 
 @Component({
     selector: 'app-user-profile',
@@ -11,14 +15,108 @@ import { Subscription } from 'rxjs';
 export class UserProfileComponent implements OnInit, AfterViewInit, OnDestroy {
 
     userProfile: any;
+    edit: boolean = false;
+    profileForm: any;
+    profileSubmitted: boolean = false;
+    passwordForm: any;
+    passwordSubmitted: boolean = false;
+
     private userSubscription: Subscription;
     
     constructor(private loadingService: LoadingService, private userService: UserService) { }
 
+    toggleEdit(){
+        this.edit = !this.edit;
+    }
+
+    initProfileForm(user){
+        this.profileForm = new FormGroup({
+            email: new FormControl('', [
+                    Validators.required,
+                    Validators.email
+                ],
+                AvailabilityValidator.checkAvailability(this.userService)
+            ),
+            
+            firstname: new FormControl('', [
+                Validators.required
+            ]),
+            lastname: new FormControl('', [
+                Validators.required
+            ]),
+            username: new FormControl('', [
+                    Validators.required,
+                    Validators.pattern('^([a-z0-9]+(?:[ _.-][a-z0-9]+)*){5,15}$')
+                ],
+                AvailabilityValidator.checkAvailability(this.userService)
+            ),
+            country: new FormControl('', [
+                Validators.required
+            ]),
+        })
+    }
+
+    initPasswordForm(){
+        this.passwordForm = new FormGroup({
+            oldpassword: new FormControl('', [
+                Validators.required,
+                Validators.pattern('^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,15}$')
+            ]),
+            passgroup: new FormGroup({
+                password: new FormControl('', [
+                    Validators.required,
+                    Validators.pattern('^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,15}$')
+                ]),
+                repeatpassword: new FormControl('', [
+                    Validators.required
+                ])
+            }, PasswordValidator.checkPassword() ),
+        })
+    }
+
+    saveProfile(profile){
+        this.profileSubmitted = true;
+        
+        if (profile.invalid) {
+            return false;
+        }
+
+        this.userService.update(profile.value).subscribe(
+            res => {
+                this.profileSubmitted = false;
+                this.userService.set(res)
+            },
+            err => {
+                console.log(err)
+            }
+        )
+    }
+
+    changePassword(passwords){
+        this.passwordSubmitted = true;
+        
+        if (passwords.invalid) {
+            return false;
+        }
+
+        this.userService.updatePassword(passwords.controls.oldpassword.value, passwords.controls.newpassword.value).subscribe(
+            res => {
+                this.passwordSubmitted = false;
+                console.log(res)
+            },
+            err => {
+                console.log(err)
+            }
+        )
+    }
+
     ngOnInit() {
         this.userSubscription = this.userService.user.subscribe( user => {
+            this.initProfileForm(user);
             this.userProfile = user;
         });
+
+        this.initPasswordForm();
     }
 
     ngAfterViewInit() {
--- src/app/components/pages/views/user/settings/settings.component.ts
+++ src/app/components/pages/views/user/settings/settings.component.ts
@@ -1,31 +1,33 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, OnDestroy } from '@angular/core';
 
 import { LoadingService } from '../../../../../services/loading.service';
+import { UserService } from '../../../../../services/user.service';
+import { Subscription } from 'rxjs';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-user-settings',
     templateUrl: './settings.component.html'
 })
-export class UserSettingsComponent implements OnInit, AfterViewInit {
+export class UserSettingsComponent implements OnInit, AfterViewInit, OnDestroy {
 
     settings: any;
 
-    constructor(private loadingService: LoadingService) { }
+    private userSubscription: Subscription;
+
+    constructor(private loadingService: LoadingService, private userService: UserService) { }
 
     ngOnInit() {
-        // This is static data just to display replace with your data
-        this.settings = {
-            emailNotification: true,
-            messageNotification: true,
-            streamingQuality: 'Very high',
-            commonVolume: true,
-            volumeLevel: 'Quiet',
-            nightMode: false
-        };
+        this.userSubscription = this.userService.user.subscribe( user => {
+            this.settings = user.settings;
+        });
     }
 
     ngAfterViewInit() {
         this.loadingService.stopLoading();
     }
 
+    ngOnDestroy() {
+        this.userSubscription.unsubscribe();
+    }
 }
