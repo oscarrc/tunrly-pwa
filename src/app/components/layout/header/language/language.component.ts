57823c7f2cc087cf769c991982633303276b6a6b
Oscar <oscarrc.web@gmail.com>
2020-10-16 20:03:51 +0200

    [MOD] Uppdate user language preference

--- src/app/components/layout/header/language/language.component.ts
+++ src/app/components/layout/header/language/language.component.ts
@@ -1,17 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { SimpleModalComponent } from 'ngx-simple-modal';
+import { UserService } from 'src/app/services/user.service';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-language',
     templateUrl: './language.component.html'
 })
 export class LanguageComponent extends SimpleModalComponent<any, any> implements OnInit {
+    private userSubscription: Subscription;
+    lang:string;
 
-    constructor() {
+    constructor(private userService: UserService) {
         super();
     }
 
+    setLanguage(lang){
+        this.userService.update({language: lang}).subscribe(
+            res => { this.userService.set(res) },
+            err => {}
+        )
+    }
+
     ngOnInit() {
+        this.userService.user.subscribe(
+            user => {this.lang = user.language}
+        )
     }
 
 }
