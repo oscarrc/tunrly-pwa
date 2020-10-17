bc9c534228f55df6e3b4862c091432c3ea13aab3
Oscar <oscarrc.web@gmail.com>
2020-10-17 12:15:08 +0200

    [MOD] User favorites component logic

--- src/app/components/pages/views/favorites/favorites.component.ts
+++ src/app/components/pages/views/favorites/favorites.component.ts
@@ -1,43 +1,79 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, OnDestroy } from '@angular/core';
 
 import { LoadingService } from '../../../../services/loading.service';
-import { SongsConfigService } from '../../../../services/songs-config.service';
+import { UserService } from 'src/app/services/user.service';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-favorites',
     templateUrl: './favorites.component.html'
 })
-export class FavoritesComponent implements OnInit, AfterViewInit {
+export class FavoritesComponent implements OnInit, AfterViewInit, OnDestroy {
 
-    favoriteSongs: any = {};
     songs: any = {};
-    gridView = false;
+    favorites: any = {};
+    gridView:boolean = false;
+    limit:number = 10;
+    page = {
+        artist: 1,
+        album: 1,
+        track: 1,
+        playlist: 1
+    }
+
+    private userSubscription: Subscription;
 
     constructor(private loadingService: LoadingService,
-                private songsConfigService: SongsConfigService) { }
+                private userService: UserService) { }
 
     ngOnInit() {
-        this.initSongs();
-        this.initFavoriteSongs();
+        this.userSubscription = this.userService.user.subscribe(
+            user => {
+                this.favorites = user.favorite;
+                this.initSongs();
+            }
+        );
     }
 
     ngAfterViewInit() {
         this.loadingService.stopLoading();
     }
 
-    // Initialize favorite songs
-    initFavoriteSongs() {
-        this.favoriteSongs.list = this.songsConfigService.songsList;
-        this.favoriteSongs.record = 5124;
+    ngOnDestroy(){
+        this.userSubscription.unsubscribe();
     }
 
+    getSimilar(){
+        let tracks = [];
+
+        this.favorites.track.forEach(track => {
+            tracks = tracks.concat(track.similar);    
+        });
+
+        return tracks;
+    }
+
+    nextPage(type){
+        const count = this.favorites[type].length;
+
+        if((count / this.limit) < this.page[type]){
+            this.page[type]++
+        }
+    }
+
+    prevPage(type){
+        if(this.page[type] > 1){
+            this.page[type]--
+        }
+    }
+   
     // Initialize song object for section
     initSongs() {
         this.songs = {
             title: 'Also Like',
             subTitle: 'Check it out these songs',
             page: '/songs',
-            items: this.songsConfigService.songsList
+            items: this.getSimilar()
         };
     }
 
