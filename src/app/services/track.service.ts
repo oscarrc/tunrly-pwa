fd515aa9adc323ab956e3af56637b90e5a38fa48
Oscar <oscarrc.web@gmail.com>
2020-10-21 19:27:40 +0200

    [DEL] Unused imports

--- src/app/services/track.service.ts
+++ src/app/services/track.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http'
-import { Observable, throwError } from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
 
 @Injectable()
 export class TrackService {
