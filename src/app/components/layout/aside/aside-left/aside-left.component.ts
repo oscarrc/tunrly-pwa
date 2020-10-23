99a332bcfc50c3c94db4a77565f2428a5ec02c1d
Oscar <oscarrc.web@gmail.com>
2020-10-23 20:04:33 +0200

    [FIX] Missing sidebar accent color

--- src/app/components/layout/aside/aside-left/aside-left.component.ts
+++ src/app/components/layout/aside/aside-left/aside-left.component.ts
@@ -2,8 +2,6 @@ import {Component, Inject, OnDestroy, OnInit} from '@angular/core';
 import { Subscription } from 'rxjs';
 
 import { MenuConfigService } from '../../../../services/menu.service';
-import { SkinService } from '../../../../services/skin.service';
-import { LocalStorageService } from '../../../../services/local-storage.service';
 import { Config } from '../../../../config/config';
 import {DOCUMENT} from '@angular/common';
 
@@ -20,9 +18,7 @@ export class AsideLeftComponent implements OnInit, OnDestroy {
     skinSubscription: Subscription;
 
     constructor(@Inject(DOCUMENT) private document: Document,
-                public menuConfigService: MenuConfigService,
-                private localStorageService: LocalStorageService,
-                private skinService: SkinService) {
+                public menuConfigService: MenuConfigService) {
         this.menuItems = this.menuConfigService.menuItems;
 
         this.asideFooterButton = {
@@ -31,19 +27,7 @@ export class AsideLeftComponent implements OnInit, OnDestroy {
         };
     }
 
-    ngOnInit() {
-        const themeSkin = this.localStorageService.getThemeSkin();
-        
-        if (themeSkin) {
-            this.sidebarClass = 'sidebar-' + Config.THEME_CLASSES[themeSkin.sidebar];
-        }
-
-        this.skinSubscription = this.skinService.themeSkin.subscribe((skin) => {
-            if (skin == 'dark') {
-                this.sidebarClass = 'sidebar-' + Config.THEME_CLASSES[skin.sidebar];
-            }
-        });
-    }
+    ngOnInit() {}
 
     hideSidebar() {
         this.document.body.classList.remove(Config.classes.openSidebar);
