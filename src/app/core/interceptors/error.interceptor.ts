960867a1f49a5ec86545cf7a3bb0899b8a00660b
Oscar <oscarrc.web@gmail.com>
2020-10-28 19:15:45 +0100

    [FIX] Multiple attemps of token renewal

--- src/app/core/interceptors/error.interceptor.ts
+++ src/app/core/interceptors/error.interceptor.ts
@@ -21,7 +21,8 @@ export class ErrorInterceptor implements HttpInterceptor {
         const refreshObservable = this.authService.refresh();
         const refreshSubject = new ReplaySubject<any>(1);
 
-        refreshSubject.subscribe( () => { this.refreshing = false })
+        refreshSubject.subscribe().add( () => { this.refreshing = false } );
+
         refreshObservable.subscribe(refreshSubject);
         
         return refreshSubject;
@@ -33,13 +34,15 @@ export class ErrorInterceptor implements HttpInterceptor {
                 case 401:
                     if(this.authService.loggedIn && !this.refreshing){                                                        
                         this.refreshing = true;
-                        return this.handleRefresh().pipe(mergeMap(() => {
-                            return this.jwtInterceptor.intercept(req, next);
-                        }))
-                    }else{
-                        this.refreshing = false;
-                        this.router.navigate(['/']);
-                        return throwError(err);
+                        return this.handleRefresh().pipe(
+                            mergeMap(() => { 
+                                return this.jwtInterceptor.intercept(req, next);
+                            }),
+                            catchError( err => {
+                                this.router.navigate(['/']);
+                                return throwError(err);
+                            })
+                        )
                     }
                 case 403:
                     // this.router.navigate(['/home']);
