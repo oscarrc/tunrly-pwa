5c81fd6d276d9b76a3586ccb5cec8578ea0f5589
Oscar <oscarrc.web@gmail.com>
2020-11-04 20:07:16 +0100

    [MOD] Language selection functionality

--- src/app/components/layout/header/language/language.component.ts
+++ src/app/components/layout/header/language/language.component.ts
@@ -1,31 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { SimpleModalComponent } from 'ngx-simple-modal';
 import { UserService } from 'src/app/services/user.service';
 import { Subscription } from 'rxjs';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
     selector: 'app-language',
     templateUrl: './language.component.html'
 })
-export class LanguageComponent extends SimpleModalComponent<any, any> implements OnInit {
+export class LanguageComponent extends SimpleModalComponent<any, any> implements OnInit, OnDestroy {
     private userSubscription: Subscription;
     lang:string;
 
-    constructor(private userService: UserService) {
+    constructor(private userService: UserService, private translateService: TranslateService) {
         super();
     }
 
     setLanguage(lang){
         this.userService.update({language: lang}).subscribe(
-            res => { this.userService.set(res) },
+            res => { 
+                this.userService.set(res);
+                this.translateService.use(this.lang);
+            },
             err => {}
         )
     }
 
     ngOnInit() {
         this.userService.user.subscribe(
-            user => {this.lang = user.language}
+            user => { 
+                this.lang = user.language 
+                this.translateService.use(this.lang);
+            }
         )
+    }
+
+    ngOnDestroy(){
+        this.userSubscription?.unsubscribe();
     }
 
 }
