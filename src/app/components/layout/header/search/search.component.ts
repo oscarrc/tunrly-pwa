abd62d0d1fe2f7cbd14db131436226abe3c4d19e
Oscar <oscarrc.web@gmail.com>
2020-11-25 16:56:30 +0100

    [MOD] Update search loading status

--- src/app/components/layout/header/search/search.component.ts
+++ src/app/components/layout/header/search/search.component.ts
@@ -32,7 +32,7 @@ export class SearchComponent implements OnInit, OnDestroy {
         });
 
         this.statusSubscription = this.searchService.searchStatus.subscribe((value) => {
-            this.loading = true;
+            this.loading = value;
         })
     }
 
