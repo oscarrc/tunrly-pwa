f64500658f92fc884fb204119484e549f1bee4b1
Oscar <oscarrc.web@gmail.com>
2020-12-05 13:38:29 +0100

    [MOD] Ad placement

--- src/app/components/layout/aside/aside-left/aside-left.component.ts
+++ src/app/components/layout/aside/aside-left/aside-left.component.ts
@@ -1,4 +1,4 @@
-import {Component, Inject, OnDestroy,} from '@angular/core';
+import {Component, Inject, OnDestroy, ElementRef, AfterViewInit, ViewChild,} from '@angular/core';
 import { Subscription } from 'rxjs';
 
 import { MenuConfigService } from 'src/app/services/menu.service';
@@ -9,13 +9,15 @@ import {DOCUMENT} from '@angular/common';
     selector: 'app-aside-left',
     templateUrl: './aside-left.component.html'
 })
-export class AsideLeftComponent implements OnDestroy {
+export class AsideLeftComponent implements AfterViewInit, OnDestroy {
 
     menuItems: any = [];
     asideFooterButton: any = {};
     sidebarClass:string = 'sidebar-primary';
 
     skinSubscription: Subscription;
+
+    @ViewChild("bannerAd") banner: ElementRef;
 
     constructor(@Inject(DOCUMENT) private document: Document,
                 public menuConfigService: MenuConfigService) {
@@ -29,6 +31,13 @@ export class AsideLeftComponent implements OnDestroy {
 
     hideSidebar() {
         this.document.body.classList.remove(Config.classes.openSidebar);
+    }
+
+    ngAfterViewInit(){
+        const script = this.document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = '//p419738.clksite.com/adServe/banners?tid=419738_823520_0';
+        this.banner.nativeElement.appendChild(script);
     }
 
     ngOnDestroy() {
