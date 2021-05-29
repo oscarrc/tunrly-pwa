efb7b52551941eab5cdcd973419768d4e534f648
Oscar <oscarrc.web@gmail.com>
2021-05-29 13:32:22 +0200

    [MOD] Retrieve last played song on reload

--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -30,6 +30,10 @@ export class PlayerService {
         return this.playlist.tracks[this.options.index];
     }
 
+    get index(){
+        return this.options.index;
+    }
+
     get playerOptions():any{
         return this.options;
     }
@@ -44,26 +48,30 @@ export class PlayerService {
         this.storageService.setLocalStorage('playlist', this.playlist);
         this.options.index = 0;
         this.options.modified = this.playlist.tracks.length > 1 ? true : false;
-        this.currentOptions.emit(this.options);
+        this.currentOptions.emit(this.options);        
+        this.storageService.setLocalStorage('player', this.options);
     }
 
     playNext(){
         if(this.options.repeat && this.options.index == this.playlist.tracks.length - 1) this.options.index = -1;
         if(this.options.shuffle) this.options.index = Math.floor(Math.random()* this.playlist.tracks.length);
         if(this.playlist.tracks?.length > this.options.index) this.options.index += 1;         
-        this.currentOptions.emit(this.options);
+        this.currentOptions.emit(this.options);        
+        this.storageService.setLocalStorage('player', this.options);
     }
 
     playPrev(){
         if(this.options.repeat && this.options.index == 0 ) this.options.index = this.playlist.tracks.length;
         if(this.options.index > 0 && this.playlist.tracks.length)          
             this.options.index = this.options.shuffle ? Math.floor(Math.random()* this.playlist.tracks.length) : this.options.index - 1;
-        this.currentOptions.emit(this.options);
+        this.currentOptions.emit(this.options);        
+        this.storageService.setLocalStorage('player', this.options);
     }
 
     playIndex(index: number){
         this.options.index = index;
-        this.currentOptions.emit(this.options);
+        this.currentOptions.emit(this.options);        
+        this.storageService.setLocalStorage('player', this.options);
     }
 
     addToPlaylist(track){
@@ -71,7 +79,8 @@ export class PlayerService {
         this.currentPlaylist.next(this.playlist);
         this.storageService.setLocalStorage('playlist', this.playlist);
         this.options.modified = this.playlist.tracks.length > 1 ? true : false;
-        this.currentOptions.emit(this.options);
+        this.currentOptions.emit(this.options);        
+        this.storageService.setLocalStorage('player', this.options);
     }
 
     playNowPlaylist(playlist){
@@ -80,7 +89,8 @@ export class PlayerService {
         this.storageService.setLocalStorage('playlist', this.playlist);     
         this.options.modified = this.playlist.tracks.length > 1 ? true : false;
         this.options.index = 0;
-        this.currentOptions.emit(this.options);
+        this.currentOptions.emit(this.options);        
+        this.storageService.setLocalStorage('player', this.options);
     }
 
     removeFromPlaylist(index){
@@ -88,7 +98,8 @@ export class PlayerService {
         this.currentPlaylist.next(this.playlist);        
         this.storageService.setLocalStorage('playlist', this.playlist);
         this.options.modified = this.playlist.tracks.length > 1 ? true : false;
-        this.currentOptions.emit(this.options);
+        this.currentOptions.emit(this.options);        
+        this.storageService.setLocalStorage('player', this.options);
     }
 
     clearPlaylist(){
