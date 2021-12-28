3275ce669172c670c61056244559cbfd56ad560a
Oscar <oscarrc.web@gmail.com>
2021-12-28 19:35:09 +0100

    [FIX] Track next/prev

--- src/app/components/layout/player/player.component.ts
+++ src/app/components/layout/player/player.component.ts
@@ -170,8 +170,7 @@ export class PlayerComponent implements OnInit, OnDestroy {
                     this.mediaSession.playbackState = "paused";
                 }).catch();
                 break;
-            case 3: //Buffering       
-                this.playPause();               
+            case 3: //Buffering                 
                 break;
             case 5: //Queued      
                 this.playPause();
@@ -263,12 +262,9 @@ export class PlayerComponent implements OnInit, OnDestroy {
         clearInterval(this.timer);        
         // @ts-ignore 
         this.mediaSession.metadata = new MediaMetadata({});     
-        this.dummy.play().then(() => {
-            this.dummy.pause() 
-            this.player.stopVideo();
-        });   
+        this.dummy.play().then(() => this.dummy.pause());   
         this.mediaSession.playbackState = "paused";
-        this.state = - 1;
+        this.state = -1;
         this.time = 0;
         this.duration = 0;
         this.buffered = 0;
